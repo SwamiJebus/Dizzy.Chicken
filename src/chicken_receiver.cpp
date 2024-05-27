166d9a6e7b883b39d04424481dc07e79ccda33e6
Stink-Jebb <70412105+Stink-Jebb@users.noreply.github.com>
2024-05-27 17:41:25 -0500

    Added DSHOT library

--- src/chicken_receiver.cpp
+++ src/chicken_receiver.cpp
@@ -1,7 +1,14 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <esp_now.h>
+#include <math.h>
 #include "rooster_protocol.h"
+
+////////////////////////
+//    Pin Mappings    //
+////////////////////////
+
+gpio_num_t Pin_ESC0 = GPIO_NUM_0;
 
 /////////////////////
 //    Variables    //
@@ -10,12 +17,14 @@
 int InputX_Raw = 0; // Analog values range from 0-4095
 int InputY_Raw = 0;
 
-// Display for health of outgoing packets.
-String TransmitHealth;
+// Enum for health of outgoing packets.
+esp_now_send_status_t TransmitHealth;
 
 ///////////////////
 //    Defines    //
 ///////////////////
+
+int AnalogDeadband = 150;
 
 uint8_t SelfAddress[] = {0xA0, 0x76, 0x4E, 0x40, 0x2E, 0x14}; // TODO Maybe don't hardcode this
 uint8_t RemoteAddress[] = {0x34, 0x85, 0x18, 0x03, 0x9b, 0x84}; // TODO Maybe don't hardcode this
@@ -34,14 +43,7 @@ esp_now_peer_info_t PeerInfo;
 
 // Callback when data is sent.
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-  if (status == 0){
-    TransmitHealth = "Healthy";
-  }
-  else{
-    TransmitHealth = "!!WARN!!";
-  }
+  TransmitHealth = status;
 }
 
 // Callback when data is received
@@ -86,19 +88,12 @@ void setup() {
     return;
   }
 
+  ////// Initialize ESCs //////
+
 }
 
 void loop() {
   BuildTelemetryPacket();
   esp_err_t result = esp_now_send(RemoteAddress, (uint8_t *) &OutgoingPacket, sizeof(OutgoingPacket)); 
-  Serial.print("Input X = ");
-  Serial.println(InputX_Raw);
-  Serial.print("Input Y = ");
-  Serial.println(InputY_Raw);
-  delay(100);
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(10);
 }
